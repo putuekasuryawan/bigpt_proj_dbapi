7ad54bf44703f996102df28ce07b4272b04ba68c
putuekasuryawan <ptekasuryawan@gmail.com>
2025-07-16 15:07:29 +0800

    Add files via upload

---
+++ Controllers/BiGptApiController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using BiGptWebApi.Helpers;
+using Npgsql;
+using MySqlConnector;
+
+namespace BiGptWebApi.Controllers
+{
+    [ApiKey]
+    public class BiGptApiController : Controller
+    {
+        [HttpGet]
+        public JsonResult SqlServerQuery(string conn, string q)
+        {
+            if (q == null || q == "")
+            {
+                var result = "Your Query or Command is blank. Please write something.";
+                return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+            }
+            else
+            {
+                using (SqlConnection db = new SqlConnection())
+                {
+                    db.ConnectionString = conn;
+
+                    try
+                    {
+                        db.Open();
+                        using (SqlCommand cmd = new SqlCommand(q, db))
+                        {
+                            cmd.CommandTimeout = 180;
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                DataTable dt = new DataTable();
+                                dt.Load(reader);
+                                var result = GetTableRows(dt);
+                                return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+                            }
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        var result = "Your Query or Command has been failed to execute. Message : " + ex.Message;
+                        return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+                    }
+                }
+            }
+        }
+        [HttpGet]
+        public JsonResult PostgreQuery(string conn, string q)
+        {
+            if (q == null || q == "")
+            {
+                var result = "Your Query or Command is blank. Please write something.";
+                return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+            }
+            else
+            {
+                using (NpgsqlConnection db = new NpgsqlConnection())
+                {
+                    db.ConnectionString = conn;
+
+                    try
+                    {
+                        db.Open();
+                        using (NpgsqlCommand cmd = new NpgsqlCommand(q, db))
+                        {
+                            cmd.CommandTimeout = 180;
+                            using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                DataTable dt = new DataTable();
+                                dt.Load(reader);
+                                var result = GetTableRows(dt);
+                                return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+                            }
+                        }
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        var result = "Your Query or Command has been failed to execute. Message : " + ex.Message;
+                        return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+                    }
+                }
+            }
+        }
+        [HttpGet]
+        public JsonResult MySqlQuery(string conn, string q)
+        {
+            if (q == null || q == "")
+            {
+                var result = "Your Query or Command is blank. Please write something.";
+                return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+            }
+            else
+            {
+                using (MySqlConnection db = new MySqlConnection())
+                {
+                    db.ConnectionString = conn;
+
+                    try
+                    {
+                        db.Open();
+                        using (MySqlCommand cmd = new MySqlCommand(q, db))
+                        {
+                            cmd.CommandTimeout = 180;
+                            using (MySqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                DataTable dt = new DataTable();
+                                dt.Load(reader);
+                                var result = GetTableRows(dt);
+                                return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+                            }
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        var result = "Your Query or Command has been failed to execute. Message : " + ex.Message;
+                        return Json(result, new Newtonsoft.Json.JsonSerializerSettings());
+                    }
+                }
+            }
+        }
+        public List<Dictionary<string, object>> GetTableRows(DataTable dtData)
+        {
+            List<Dictionary<string, object>>
+            listRows = new List<Dictionary<string, object>>();
+
+            foreach (DataRow dr in dtData.Rows)
+            {
+                Dictionary<string, object> dictRow = new Dictionary<string, object>();
+                foreach (DataColumn col in dtData.Columns)
+                {
+                    dictRow.Add(col.ColumnName, dr[col]);
+                }
+                listRows.Add(dictRow);
+            }
+            return listRows;
+        }
+    }
+}
